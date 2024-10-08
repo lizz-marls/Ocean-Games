a049f667089841dec148446f4633bd196e0bcb4f
bryn32 <148895662+bryn32@users.noreply.github.com>
2024-10-08 14:24:18 -0700

    Update bubblePop-sketch.js

--- sketches/bubblePop/bubblePop-sketch.js
+++ sketches/bubblePop/bubblePop-sketch.js
@@ -1,1 +1,18 @@
+var bubbbleBackground;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100);
+  
+}
+
+function draw() {
+  imageMode(CENTER);
+  image(bubbleBackground, width/2, height/2, width, height);
+
+}
+
+function preload() {
+  bubbleBackground = loadImage('gameBackground2.png');
+  
+}
