59aea62fa8aea72fa06f0a3ba758eff3bae050f7
Lexie Midtun <173127209+lexiemidtun@users.noreply.github.com>
2024-10-08 17:59:13 -0700

    Update colorMix-sketch.js

--- sketches/ColorMix/colorMix-sketch.js
+++ sketches/ColorMix/colorMix-sketch.js
@@ -1,1 +1,28 @@
+var landscape;
+var w;
+var h;
 
+function preload() {
+  // Preload the image to ensure it's fully loaded before using it
+  landscape = loadImage("../../assets/gameBackground1.png");
+}
+
+function setup() {
+  createCanvas(windowWidth-5, windowHeight-5);
+
+  w = width-10;
+  h = height-10;
+
+  background(100);
+  
+}
+
+function draw() {
+  // Check if the image is loaded before trying to display it
+  if (landscape) {
+    imageMode(CORNER);
+    image(landscape, 5, 5, w, h);
+  } else {
+    console.log('Image not loaded');
+  }
+}
