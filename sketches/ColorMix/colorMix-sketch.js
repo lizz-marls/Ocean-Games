aeacd75f66dbefa17a8c3f2b7105f06d0b79d280
Lexie Midtun <173127209+lexiemidtun@users.noreply.github.com>
2024-10-08 20:28:43 -0700

    Update colorMix-sketch.js

--- sketches/ColorMix/colorMix-sketch.js
+++ sketches/ColorMix/colorMix-sketch.js
@@ -31,20 +31,4 @@ function draw() {
   }
 }
 
-function buildHomeButton(){
-  let button = createButton('HOME');
-  
-  // Correct position and size of the button
-  button.position(windowWidth * 0.43, windowHeight * 0.9); 
-  button.size(165, 165);  // Adjust button size
 
-  // Apply CSS styles
-  button.style('background-color', 'white');
-  button.style('border', 'black');
-  button.style('border-radius', '75%');  // Circle button
-  
-  // Redirect on button click
-  button.mousePressed(() => {
-    //window.location.href = "../../index.html";
-  });
-}
