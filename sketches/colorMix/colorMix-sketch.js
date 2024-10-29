36941d0c2091534d893acc7d76120993e07515a9
Lexie Midtun <173127209+lexiemidtun@users.noreply.github.com>
2024-10-29 00:04:54 -0700

    Update colorMix-sketch.js

--- sketches/colorMix/colorMix-sketch.js
+++ sketches/colorMix/colorMix-sketch.js
@@ -21,12 +21,14 @@ function setup() {
 
   background(100);
   
-  buildColorButton("redButton", "red", w/2-375, h/2+100 );
-  buildColorButton("blueButton", "blue", w/2-175, h/2+100 );
-  buildColorButton("greenButton", "green", w/2+25, h/2+100 );
-  buildColorButton("whiteButton", "white", w/2+225, h/2+100 );
+  buildColorButton("redButton", "red", w/2-300, h/2+100 );
+  buildColorButton("blueButton", "blue", w/2-100, h/2+100 );
+  buildColorButton("greenButton", "green", w/2+100, h/2+100 );
+  buildColorButton("whiteButton", "white", w/2+300, h/2+100 );
+  
+  buildResetButton(); 
 
-  buildHomeButton();
+  buildHomeButton(); 
 }
 
 function draw() {
@@ -47,19 +49,38 @@ function draw() {
   
   triangle(w/2-50, h/2-270, w/2-110,h/2-160, w/2-20, h/2-160);
   
-  ellipse((w/2)-20, (h/2)-100, 350, 200);
+  ellipse((w/2)-20, (h/2)-100, 350, 220);
   
   triangle(w/2+80, h/2-100, w/2-20, h/2-80, w/2-20, h/2-130);
-
-  fill("white");
+  
   circle(w/2-135, h/2-130, 30);
   
   fill(0, 0, 0);
   circle(w/2-137, h/2-132, 15);
   
   fill(r, g, b);
-  arc(w/2-153, h/2-90, 80, 80, 0 + 2*HALF_PI/5 , HALF_PI+HALF_PI/2);
+  arc(w/2-155, h/2-90, 80, 80, 0 + 2*HALF_PI/5 , HALF_PI+HALF_PI/2);
   
+}
+
+function buildResetButton(){
+  let button = createButton('RESET'); //button text 
+  button.position(w-155, 5); //button position on screen
+  button.size(150, 75);  // button size
+
+  //button properties
+  button.style('background-color', 'white'); //button color
+  button.style('border', '3px solid black'); 
+  button.style('border-radius', '10px');  //rounds corners
+  button.style('font-size', '36px') ;
+  button.style('font-weight', 'bold');
+  
+  // redirect home on button click
+  button.mousePressed(() => {
+    r =255;
+    b = 255;
+    g=255;
+  });
 }
 
 function buildColorButton(name, color, x, y){
@@ -73,18 +94,18 @@ function buildColorButton(name, color, x, y){
                
   button.mousePressed(() => {
     if(name === "redButton"){
-    b-= 10;
-    g -=10;
+    b-= 15;
+    g -=15;
   } else if (name === "blueButton"){
-    g -= 10;
-    r -= 10;
+    g -= 15;
+    r -=15;
   } else if (name === "greenButton"){
-    r-= 10;
-    b -= 10;
+    r-= 15;
+    b -=15;
   } else{
-    r+= 10;
-    g += 10;
-    b += 10;
+    r+= 15;
+    g += 15;
+    b += 15;
   }
     
   });
