28014384dc955a89bab6d155161d3e7720e1553d
lizzie <emmarisc@asu.edu>
2024-10-30 22:38:11 -0700

    Update animalSounds-sketch.js
    load and play sounds, speaker button and animal buttons,

--- sketches/animalSounds/animalSounds-sketch.js
+++ sketches/animalSounds/animalSounds-sketch.js
@@ -2,9 +2,23 @@ var landscape;
 var w;
 var h;
 
+let sounds = [];
+let animalNames = ['Dog', 'Cat', 'Chicken', 'Pig', 'Cow'];
+let currentSoundIndex = -1;
+let canClick = false;
+let feedbackColor = 'white';
+let speakerButton, homeButton, animalButtons = [];
+
 //load image in
 function preload() {
   landscape = loadImage("../../assets/gameBackground1.png");
+  
+  //sounds
+    sounds[0] = loadSound('sounds/dog.mp3');
+    sounds[1] = loadSound('sounds/cat.mp3');
+    sounds[2] = loadSound('sounds/chicken.mp3');
+    sounds[3] = loadSound('sounds/pig.mp3');
+    sounds[4] = loadSound('sounds/cow.mp3');
 }
 
 
@@ -17,6 +31,22 @@ function setup() {
   background(100);
 
   buildHomeButton();
+
+  //speaker button:
+    speakerButton = createButton('🎤'); //temporary emoji!
+    speakerButton.position(width / 2 - 25, height / 2 + 50);
+    speakerButton.size(50);
+    speakerButton.mousePressed(playSound);
+  //animal buttons:
+    for (let i = 0; i < animalNames.length; i++) {
+        let animalButton = createButton(animalNames[i]);
+        animalButton.position(width / 2 + 100 * cos(PI / 5 * i) - 25, height / 2 - 100 * sin(PI / 5 * i) + 50);
+        animalButton.size(70);
+        animalButton.mousePressed(() => checkAnswer(i));
+        animalButtons.push(animalButton);
+    }
+    
+    noLoop();  
 }
 
 function draw() {
@@ -28,7 +58,45 @@ function draw() {
   } else {
     console.log('Image not loaded');
   }
+  //button colors
+    fill(feedbackColor);
+    ellipse(width / 2, height / 2 + 50, 150, 150);
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    fill(0);
+    text('Click the Speaker!', width / 2, height / 2);
   
+}
+
+function playSound() {
+    if (currentSoundIndex < 0) {
+        currentSoundIndex = floor(random(sounds.length));
+        sounds[currentSoundIndex].play();
+        feedbackColor = 'white';
+        canClick = true;
+        setTimeout(() => {
+            canClick = false;
+        }, 1000);
+    }
+}
+
+function checkAnswer(index) {
+    if (canClick) {
+        if (index === currentSoundIndex) {
+            animalButtons[index].style('background-color', 'green');
+            setTimeout(() => {
+                animalButtons[index].style('background-color', 'white');
+                currentSoundIndex = -1; // Reset sound index
+                canClick = false; // Reset ability to click
+            }, 1000);
+        } else {
+            animalButtons[index].style('background-color', 'red');
+            setTimeout(() => {
+                animalButtons[index].style('background-color', 'white');
+                canClick = false; // Reset ability to click
+            }, 3000);
+        }
+    }
 }
 
 function buildHomeButton(){
@@ -50,3 +118,5 @@ function buildHomeButton(){
     window.location.href = "../../index.html"; 
   });
 }
+
+
