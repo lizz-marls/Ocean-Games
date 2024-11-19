f1ded6c9068086d4f61b4b2612587d2178f2d351
Lexie Midtun <173127209+lexiemidtun@users.noreply.github.com>
2024-11-19 12:51:10 -0800

    Update shapeMatch-sketch.js

--- sketches/shapeMatch/shapeMatch-sketch.js
+++ sketches/shapeMatch/shapeMatch-sketch.js
@@ -1,4 +1,8 @@
-let bubbleBackground;
+var landscape;
+var w;
+var h;
+var r, g, b;
+
 const targetShapeY = 250;
 const targetShapeY2 = 500;
 let shuffledCoords;
@@ -6,10 +10,17 @@ let targetShapeCoords;
 let shapes = [];
 let draggedShape = null;
 
+function preload() {
+  landscape = loadImage("../../assets/gameBackground1.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100);
 
+  w = width;
+  h = height;
+
+  background(100);
   
   targetShapeCoords = getTargetShapeCoords();
 
@@ -27,13 +38,18 @@ function setup() {
       matched: false,
     });
   }
+
+  buildHomeButton(); 
 }
 
 function draw() {
-  imageMode(CENTER);
-  image(bubbleBackground, width / 2, height / 2);
+  if (landscape) {
+    imageMode(CORNER);
+    image(landscape, 0, 0, w, h);
+  } else {
+    console.log('Image not loaded');
+  }
 
-  rectMode(CENTER);
 
  
   drawTargetShapes(targetShapeCoords);
@@ -212,6 +228,23 @@ function shuffleArray(arr) {
   return arr;
 }
 
-function preload() {
-  bubbleBackground = loadImage("Copy of Master Project GUI Story-Board.png");
+function buildHomeButton(){
+  
+  //create button
+  let button = createButton('HOME'); //button text 
+   button.position(w-155, 5); //button position on screen
+  button.size(150, 75);  // button size
+
+  //button properties
+  button.style('background-color', 'white'); //button color
+  button.style('border', '3px solid black'); 
+  button.style('border-radius', '10px');  //rounds corners
+  button.style('font-size', '36px') ;
+  button.style('font-weight', 'bold');
+  
+  // redirect home on button click
+  button.mousePressed(() => {
+    window.location.href = "../../index.html"; 
+  });
+
 }
