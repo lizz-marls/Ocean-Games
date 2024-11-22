bfcc0ad1227e497cdd5dd73fe0db8ad6d6bfda2a
Lexie Midtun <173127209+lexiemidtun@users.noreply.github.com>
2024-11-21 21:28:15 -0700

    Update shapeMatch-sketch.js

--- sketches/shapeMatch/shapeMatch-sketch.js
+++ sketches/shapeMatch/shapeMatch-sketch.js
@@ -262,7 +262,7 @@ function buildHomeButton() {
 function buildResetButton() {
   let button = createButton('PLAY AGAIN');
   button.id('resetButton');
-  button.position((w - 400) * 0.5, h * 0.7);
+  button.position((w - 400) * 0.5, h * 0.6);
   button.size(400, 75);
 
   // Button properties
